0749f8a2cfd9ac92d61886e0ef344aa6becf0822
Batuhan Özhan <batuhanozhan00@gmail.com>
2024-02-11 02:07:52 +0300

    Initial Commit

---
+++ spotify-clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spotify-clone
+//
+//  Created by Batuhan Özhan on 11.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spotify-clone/spotify_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotify_cloneApp.swift
+//  spotify-clone
+//
+//  Created by Batuhan Özhan on 11.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct spotify_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
