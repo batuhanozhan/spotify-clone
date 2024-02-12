34f2b5a846b9c03e924a2744ac53092ad348ad0c
Batuhan Özhan <batuhanozhan00@gmail.com>
2024-02-12 05:34:10 +0300

    commit

--- spotify-clone/Login Page/Buttons.swift
+++ spotify-clone/Login Page/Buttons.swift
@@ -6,13 +6,35 @@
 //
 
 import SwiftUI
-
-struct Buttons: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//Green Button
+struct LoginButtons: View {
+    
+    var buttonText: String
+    var buttonColor: String
+    var buttonTextColor: String
+    var buttonBorder: Bool
+    var action: Optional<() -> Void>
+    
+    //Login Page button style
+    
+    var body: some View{
+        Button(action: action ?? {} , label: {
+            Text(buttonText)
+            
+                .frame(width: 290, height: 18)
+                .foregroundStyle(Color(buttonTextColor))
+                .bold()
+                .padding()
+                .background(Color(buttonColor))
+                .clipShape(Capsule())
+                .overlay(RoundedRectangle(cornerRadius: 25)
+                    .stroke(buttonBorder ? Color.white : Color.clear, lineWidth: 1))
+        })
     }
 }
 
+
 #Preview {
-    Buttons()
+    LoginButtons(buttonText: "sign in", buttonColor: "SecondColor", buttonTextColor: "MainColor", buttonBorder: true, action: nil)
+    
 }
