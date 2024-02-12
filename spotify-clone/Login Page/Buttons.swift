51c6fc4cb090fe23f34c104bb17b7741126e1260
Batuhan Özhan <batuhanozhan00@gmail.com>
2024-02-12 05:29:40 +0300

    First Commit

---
+++ spotify-clone/Login Page/Buttons.swift
@@ -0,0 +1,18 @@
+//
+//  Buttons.swift
+//  spotify-clone
+//
+//  Created by Batuhan Özhan on 11.02.2024.
+//
+
+import SwiftUI
+
+struct Buttons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Buttons()
+}
---
+++ spotify-clone/Login Page/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  spotify-clone
+//
+//  Created by Batuhan Özhan on 11.02.2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
